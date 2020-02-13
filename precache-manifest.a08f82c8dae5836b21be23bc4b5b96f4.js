5210d1d8ae9eb0222c54338912513cfdf73d6254
rkbhambri <ramkrishan@redbaron.in>
2020-02-13 17:58:43 +0530

    Updates

---
+++ precache-manifest.a08f82c8dae5836b21be23bc4b5b96f4.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = [
+  {
+    "revision": "cca822657fb6be1393fece1f19bd1876",
+    "url": "/virtual-card/static/media/something-went-wrong.cca82265.gif"
+  },
+  {
+    "revision": "3c05f51f5871c91cfa64",
+    "url": "/virtual-card/static/js/0.8b3a61b0.chunk.js"
+  },
+  {
+    "revision": "ebb6fdbb8d117c017f29",
+    "url": "/virtual-card/static/js/2.306d44f6.chunk.js"
+  },
+  {
+    "revision": "0d0d3c94d05def82a219bcab4f5e683a",
+    "url": "/virtual-card/static/media/bg-login.0d0d3c94.jpg"
+  },
+  {
+    "revision": "26d0a895db1712222b81",
+    "url": "/virtual-card/static/js/main.6129cfe3.chunk.js"
+  },
+  {
+    "revision": "b20a4debb69b46296594",
+    "url": "/virtual-card/static/js/runtime~main.cd7c91d5.js"
+  },
+  {
+    "revision": "7a21cbe90fd31540fb0384792fbc4a86",
+    "url": "/virtual-card/static/media/user.7a21cbe9.svg"
+  },
+  {
+    "revision": "89df8e97b648af3d6661",
+    "url": "/virtual-card/static/js/5.dca6ee40.chunk.js"
+  },
+  {
+    "revision": "14cdb920d6ac60d40d1a",
+    "url": "/virtual-card/static/js/6.227641da.chunk.js"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/virtual-card/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "ddafcd1dfc11b5db66a3",
+    "url": "/virtual-card/static/js/7.3495d2be.chunk.js"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/virtual-card/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "64c5942791a18ee2524e",
+    "url": "/virtual-card/static/js/8.dcafa14e.chunk.js"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/virtual-card/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad1e17dc011159e291ae",
+    "url": "/virtual-card/static/js/9.83138e66.chunk.js"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/virtual-card/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "c30c1688938f14d7ae3f",
+    "url": "/virtual-card/static/js/10.31dd6326.chunk.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/virtual-card/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "f187850149ccdb536b59",
+    "url": "/virtual-card/static/js/11.3c83975d.chunk.js"
+  },
+  {
+    "revision": "2ca6b6102c554466af18",
+    "url": "/virtual-card/static/js/1.d8257a11.chunk.js"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/virtual-card/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/virtual-card/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/virtual-card/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/virtual-card/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/virtual-card/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/virtual-card/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/virtual-card/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/virtual-card/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/virtual-card/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/virtual-card/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/virtual-card/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "26d0a895db1712222b81",
+    "url": "/virtual-card/static/css/main.63c6341e.chunk.css"
+  },
+  {
+    "revision": "ad1e17dc011159e291ae",
+    "url": "/virtual-card/static/css/9.e8b861fb.chunk.css"
+  },
+  {
+    "revision": "64c5942791a18ee2524e",
+    "url": "/virtual-card/static/css/8.c43f8992.chunk.css"
+  },
+  {
+    "revision": "ddafcd1dfc11b5db66a3",
+    "url": "/virtual-card/static/css/7.ab441fb0.chunk.css"
+  },
+  {
+    "revision": "89df8e97b648af3d6661",
+    "url": "/virtual-card/static/css/5.c535fc59.chunk.css"
+  },
+  {
+    "revision": "f187850149ccdb536b59",
+    "url": "/virtual-card/static/css/11.33072daf.chunk.css"
+  },
+  {
+    "revision": "c30c1688938f14d7ae3f",
+    "url": "/virtual-card/static/css/10.506daef8.chunk.css"
+  },
+  {
+    "revision": "5a4e4280c0a69996db0c529fea58c032",
+    "url": "/virtual-card/index.html"
+  }
+];
