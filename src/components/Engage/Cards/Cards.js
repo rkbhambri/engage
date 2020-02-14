65bdf8c53e9f187c5b82a79b3859d3c61a728ee8
rkbhambri <ramkrishan@redbaron.in>
2020-02-14 15:37:50 +0530

    Added horizintal scroll

--- src/components/Engage/Cards/Cards.js
+++ src/components/Engage/Cards/Cards.js
@@ -13,97 +13,112 @@ const Cards = (props) => {
         setEditcardId(prevState => !prevState);
     };
 
+    const cards = [0, 1, 2, 3, 4, 5].map(item => {
+        return (
+            <Col key={item} className="cards" style={{ borderRadius: '15px' }} onClick={() => editCardDetails(props.id)}>
+                <Col className="text-right mb-4">
+                    <GoBroadcast className="mt-3" style={{ fontSize: '28px' }} />
+                    <div className="text-center">
+                        <div className="profile-pic">
+                            <img
+                                src={Profile_pic}
+                                className="rounded-circle"
+                                alt="not available"
+                                style={{ height: '60px', width: '60px' }}
+                            // onError={(event) => event.target.src = defaultUserImage}
+                            />
+                        </div>
+                        <div className="name mt-3">KARAMJIT DAS</div>
+                    </div>
+                </Col>
+                <Col
+                    className="social-details pb-2"
+                    md={12}
+                    sm={12}>
+                    <div className="social-icons">
+                        <Divider horizontal>
+                            <Icon name="facebook official" style={{ fontSize: '28px' }} />
+                            <Icon className="ml-2 mr-2" name="instagram" style={{ fontSize: '28px' }} />
+                            <Icon className="ml-2 mr-2" name="youtube" style={{ fontSize: '28px' }} />
+                            <Icon name="github" style={{ fontSize: '28px' }} />
+                        </Divider>
+                    </div>
+                </Col>
+                {
+                    editCardId &&
+                    <div className="p-2">
+                        <div className="d-flex" style={{ justifyContent: 'space-between' }}>
+                            <Icon name="facebook official" style={{ fontSize: '28px' }} />
+                            <Input
+                                type="text"
+                                transparent
+                                placeholder="Enter here"
+                                className="border"
+                                style={{ color: '#fff', borderRadius: '7px' }}
+                            // onChange={(event) => setMoney(event.target.value)}
+                            // value="parasbhambri.2@facebook.com"
+                            />
+                            <Radio toggle />
+                        </div>
+                        <div className="d-flex mt-4" style={{ justifyContent: 'space-between' }}>
+                            <Icon name="instagram" style={{ fontSize: '28px' }} />
+                            <Input
+                                type="text"
+                                transparent
+                                placeholder="Enter here"
+                                className="border"
+                                style={{ color: '#fff', borderRadius: '7px' }}
+                            // onChange={(event) => setMoney(event.target.value)}
+                            // value={moneyValue}
+                            />
+                            <Radio toggle />
+                        </div>
+                        <div className="d-flex mt-4" style={{ justifyContent: 'space-between' }}>
+                            <Icon name="youtube" style={{ fontSize: '28px' }} />
+                            <Input
+                                type="text"
+                                transparent
+                                placeholder="Enter here"
+                                className="border"
+                                style={{ color: '#fff', borderRadius: '7px' }}
+                            // onChange={(event) => setMoney(event.target.value)}
+                            // value={moneyValue}
+                            />
+                            <Radio toggle />
+                        </div>
+                        <div className="d-flex mt-4" style={{ justifyContent: 'space-between' }}>
+                            <Icon name="github" style={{ fontSize: '28px' }} />
+                            <Input
+                                type="text"
+                                transparent
+                                placeholder="Enter here"
+                                className="border"
+                                style={{ color: '#fff', borderRadius: '7px' }}
+                            // onChange={(event) => setMoney(event.target.value)}
+                            // value={moneyValue}
+                            />
+                            <Radio toggle />
+                        </div>
+                        <div className="text-center mt-4">
+                            <Button className="pl-4 pr-4" size="tiny" icon="check" style={{ color: 'green', borderRadius: '8px' }} />
+                        </div>
+                    </div>
+                }
+            </Col>
+        );
+    });
+
     return (
-        <Col className="cards" style={{ borderRadius: '15px' }} onClick={() => editCardDetails(props.id)}>
-            <Col className="text-right mb-4">
-                <GoBroadcast className="mt-3" style={{ fontSize: '28px' }} />
-                <div className="text-center">
-                    <div className="profile-pic">
-                        <img
-                            src={Profile_pic}
-                            className="rounded-circle"
-                            alt="not available"
-                            style={{ height: '60px', width: '60px' }}
-                        // onError={(event) => event.target.src = defaultUserImage}
-                        />
-                    </div>
-                    <div className="name mt-3">KARAMJIT DAS</div>
-                </div>
-            </Col>
-            <Col
-                className="social-details pb-2"
-                md={12}
-                sm={12}>
-                <div className="social-icons">
-                    <Divider horizontal>
-                        <Icon name="facebook official" style={{ fontSize: '28px' }} />
-                        <Icon className="ml-2 mr-2" name="instagram" style={{ fontSize: '28px' }} />
-                        <Icon className="ml-2 mr-2" name="youtube" style={{ fontSize: '28px' }} />
-                        <Icon name="github" style={{ fontSize: '28px' }} />
-                    </Divider>
-                </div>
-            </Col>
-            {
-                editCardId &&
-                <div className="p-2">
-                    <div className="d-flex" style={{ justifyContent: 'space-between' }}>
-                        <Icon name="facebook official" style={{ fontSize: '28px' }} />
-                        <Input
-                            type="text"
-                            transparent
-                            placeholder="Enter here"
-                            className="border"
-                            style={{ color: '#fff', borderRadius: '7px' }}
-                        // onChange={(event) => setMoney(event.target.value)}
-                        // value="parasbhambri.2@facebook.com"
-                        />
-                        <Radio toggle />
-                    </div>
-                    <div className="d-flex mt-4" style={{ justifyContent: 'space-between' }}>
-                        <Icon name="instagram" style={{ fontSize: '28px' }} />
-                        <Input
-                            type="text"
-                            transparent
-                            placeholder="Enter here"
-                            className="border"
-                            style={{ color: '#fff', borderRadius: '7px' }}
-                        // onChange={(event) => setMoney(event.target.value)}
-                        // value={moneyValue}
-                        />
-                        <Radio toggle />
-                    </div>
-                    <div className="d-flex mt-4" style={{ justifyContent: 'space-between' }}>
-                        <Icon name="youtube" style={{ fontSize: '28px' }} />
-                        <Input
-                            type="text"
-                            transparent
-                            placeholder="Enter here"
-                            className="border"
-                            style={{ color: '#fff', borderRadius: '7px' }}
-                        // onChange={(event) => setMoney(event.target.value)}
-                        // value={moneyValue}
-                        />
-                        <Radio toggle />
-                    </div>
-                    <div className="d-flex mt-4" style={{ justifyContent: 'space-between' }}>
-                        <Icon name="github" style={{ fontSize: '28px' }} />
-                        <Input
-                            type="text"
-                            transparent
-                            placeholder="Enter here"
-                            className="border"
-                            style={{ color: '#fff', borderRadius: '7px' }}
-                        // onChange={(event) => setMoney(event.target.value)}
-                        // value={moneyValue}
-                        />
-                        <Radio toggle />
-                    </div>
-                    <div className="text-center mt-4">
-                        <Button className="pl-4 pr-4" size="tiny" icon="check" style={{ color: 'green', borderRadius: '8px' }} />
-                    </div>
-                </div>
-            }
-        </Col>
+        <ScrollMenu
+            data={cards}
+            alignOnResize={true}
+            itemStyle={{ width: '300px' }}
+        // item
+        // arrowLeft={ArrowLeft}
+        // arrowRight={ArrowRight}
+        // selected={selected}
+        // onSelect={this.onSelect}
+        />
     );
 };
 
