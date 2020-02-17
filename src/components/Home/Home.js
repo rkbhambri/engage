660bf731ccd14e95606c5337adace1744300732f
rkbhambri <ramkrishan@redbaron.in>
2020-02-17 11:56:40 +0530

    remove console.log

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,6 @@ import SocialDetails from './SocialDetails/SocialDetails';
 import './Home.css';
 
 const Home = (props) => {
-    console.log('====check====');
     return (
         <div className="home">
             <div className="home-data pt-4">
