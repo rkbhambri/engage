9a1c2fe3684344aaabba5134946679ff22333968
rkbhambri <ramkrishan@redbaron.in>
2020-04-15 15:10:18 +0530

    added few more url

--- src/components/Home/SocialDetails/SocialDetails.js
+++ src/components/Home/SocialDetails/SocialDetails.js
@@ -29,18 +29,49 @@ const SocialDetails = (props) => {
     return (
         <Col className="social-details pt-4" xs={12}>
             <Col className="d-flex mt-4 justify-content-between p-3">
-                <SocialIcons src={Dribble} platform="dribble" value={props.socialUrl.dribbleUrl} />
-                <SocialIcons src={Facebook} platform="facebook" value={props.socialUrl.facebookUrl} updateSocialUrl={(value) => updateSocialUrl(value, 'facebookUrl')} />
-                <SocialIcons src={Linkedin} platform="linkedin" value={props.socialUrl.linkedinUrl} />
+                <SocialIcons
+                    src={Dribble}
+                    platform="dribble"
+                    value={props.socialUrl.dribbleUrl}
+                    updateSocialUrl={(value) => updateSocialUrl(value, 'dribbleUrl')} />
+                <SocialIcons
+                    src={Facebook}
+                    platform="facebook"
+                    value={props.socialUrl.facebookUrl}
+                    updateSocialUrl={(value) => updateSocialUrl(value, 'facebookUrl')} />
+                <SocialIcons
+                    src={Linkedin} platform="linkedin"
+                    value={props.socialUrl.linkedinUrl}
+                    updateSocialUrl={(value) => updateSocialUrl(value, 'linkedinUrl')} />
             </Col>
             <Col className="d-flex justify-content-around p-3">
-                <SocialIcons src={Behance} platform="behance" value={props.socialUrl.behanceUrl} />
-                <SocialIcons src={Instagram} platform="instagram" value={props.socialUrl.instagramUrl} />
+                <SocialIcons
+                    src={Behance}
+                    platform="behance"
+                    value={props.socialUrl.behanceUrl}
+                    updateSocialUrl={(value) => updateSocialUrl(value, 'behanceUrl')} />
+                <SocialIcons
+                    src={Instagram}
+                    platform="instagram"
+                    value={props.socialUrl.instagramUrl}
+                    updateSocialUrl={(value) => updateSocialUrl(value, 'instagramUrl')} />
             </Col>
             <Col className="d-flex justify-content-between p-3">
-                <SocialIcons src={Pinterest} platform="pinterest" value={props.socialUrl.pinterestUrl} />
-                <SocialIcons src={Twitter} platform="twitter" value={props.socialUrl.twitterUrl} />
-                <SocialIcons src={Youtube} platform="youtubeChannel" value={props.socialUrl.youtubeChannelUrl} />
+                <SocialIcons
+                    src={Pinterest}
+                    platform="pinterest"
+                    value={props.socialUrl.pinterestUrl}
+                    updateSocialUrl={(value) => updateSocialUrl(value, 'pinterestUrl')} />
+                <SocialIcons
+                    src={Twitter}
+                    platform="twitter"
+                    value={props.socialUrl.twitterUrl}
+                    updateSocialUrl={(value) => updateSocialUrl(value, 'twitterUrl')} />
+                <SocialIcons
+                    src={Youtube}
+                    platform="youtubeChannel"
+                    value={props.socialUrl.youtubeChannelUrl}
+                    updateSocialUrl={(value) => updateSocialUrl(value, 'youtubeChannelUrl')} />
             </Col>
         </Col>
     );
